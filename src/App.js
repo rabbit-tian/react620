40aad94c72db1dc0fee49836f031baedc799762f
rabbit-tian <1019610885@qq.com>
2018-06-20 10:20:14 +0800

    first init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h3>hello tian</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
